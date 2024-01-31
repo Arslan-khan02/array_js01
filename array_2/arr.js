eee9ae7b547c1a414eba5c93d473d7c45285c954
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-01-31 13:15:38 +0000

    complete array p2

--- array_2/arr.js
+++ array_2/arr.js
@@ -1,2 +1,35 @@
-const hero = ["ww","ll","aa"]
-console.log(hero[0]);
+// const hero = ["ww","ll","aa"]
+// console.log(hero[0]);
+// concat method
+let cold_drinks = ["fanta","sprite","lemon"]
+let hot_drinks =["coffee","tea","milk"]
+// let drinks = cold_drinks.concat(hot_drinks)
+// console.log(drinks);
+// spread methods high prefrence
+// let all_drinks =[...cold_drinks,...hot_drinks]
+// console.log(all_drinks);
+
+// complicate situation in array
+
+// let bhasad_array = [1,2,3,[4,5],6,7,8,[1,2,3,[4,5]]]
+// let real_array = bhasad_array.flat(Infinity)
+// console.log(real_array);
+
+// converting into array
+console.log(Array.isArray("khan"));
+console.log(Array.from("arslan"));//1 tarika
+console.log(Array.from({name:"love"}));//define krna pdega key value se array ya string se array
+
+let marks1 = 299
+let marks2 = 399
+let mark3 = 499
+console.log(Array.of(marks1,marks2,mark3));
+
+
+
+
+
+
+
+
+
