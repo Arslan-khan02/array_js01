55b573311e44c654926e0fc6e337770cfdee5bc9
Arslan-khan02 <154960658+Arslan-khan02@users.noreply.github.com>
2024-02-03 10:54:51 +0000

    object part on complete

--- object/obj_1.js
+++ object/obj_1.js
@@ -1,15 +1,28 @@
-  // obj literals
+// obj literals
+// symbol kese define hota hai 
+// objct freez kese krte h
+// object ki value kese change hoti hai
 
-  const mySym = Symbol("key")
-  const jsUser = {
-    name : "arslan ",
-    age : 12,
-    [mySym]: "man",
-    gender : "male", 
-  }
-  // console.log(jsUser["name"]);
-  // console.log(jsUser[mySym]);
-  jsUser.name = "urslan"
-  jsUser.Symbol = "my key"
 
-  console.log(jsUser.mySym);
+const mySym = Symbol("key")
+const jsUser = {
+  name: "arslan ",
+  age: 12,
+  [mySym]: "man",
+  gender: "male",
+}
+// Object.freeze(jsUser)
+jsUser.name = "urslan"
+
+// console.log(jsUser);
+// function kese dalte hai
+jsUser.greeting = function(){
+  console.log("hello JS user");
+
+}
+console.log(jsUser.greeting()); 
+
+jsUser.greetingTwo = function(){
+  console.log(`hello user, ${this.name}`);
+}
+console.log(jsUser.greetingTwo());
